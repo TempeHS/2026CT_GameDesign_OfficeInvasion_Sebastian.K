d43641b9e85c9b4b4447be7a7a51af08d7128f11
Sebastian K <sebastian.king19@education.nsw.gov.au>
2026-06-03 10:35:12 +1000

    WIP.healthbar

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -8,17 +8,33 @@ public class PlayerMovement : MonoBehaviour
    [SerializeField] private float moveSpeed = 5f;
    private Rigidbody2D rb;
    private Vector2 moveInput;
-   private int leftPunch;
+   private bool leftPunch;
+   private bool rightPunch;
+   public float Health, MaxHealth;
+
+   [SerializeField]
+    private HealthBarUI healthBar;
    // Start is called once before the first execution of Update after the MonoBehaviour is created
    void Start()
    {
       rb = GetComponent<Rigidbody2D>();
+      healthBar.SetMaxHealth(MaxHealth);
    }
 
    // Update is called once per frame
    void Update()
    {
        rb.linearVelocity = moveInput * moveSpeed;
+
+       if (Input.GetKeyDown("l"))
+        {
+            SetHealth(-10f);
+        }
+
+        if (Input.GetKeyDown("p"))
+        {
+            SetHealth(10f);
+        }
    }
 
    public void Move(InputAction.CallbackContext context)
@@ -26,13 +42,27 @@ public class PlayerMovement : MonoBehaviour
        moveInput = context.ReadValue<Vector2>();
    }
 
+   public void SetHealth(float healthChange)
+    {
+        Health += healthChange;
+        Health = Mathf.Clamp(Health, 0, MaxHealth);
+        healthBar.SetHealth(Health);
+    }
+
    private void Punch()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            leftPunch = 1;
+            leftPunch = true;
             StartCoroutine(PunchCooldown());
-            leftPunch = 0;
+            leftPunch = false;
+        }
+
+             if (Input.GetMouseButtonDown(1))
+        {
+            rightPunch = true;
+            StartCoroutine(PunchCooldown());
+            rightPunch = false;
         }
     }
 
