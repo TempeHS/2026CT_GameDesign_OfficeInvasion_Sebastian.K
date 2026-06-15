51e8f2798ce9b03189d3c1b776b5585a374ac9e7
Sebastian K <sebastian.king19@education.nsw.gov.au>
2026-06-15 11:40:28 +1000

    WIPdfjk

--- Assets/scripts/Attack.cs
+++ Assets/scripts/Attack.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    private Animator anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetTrigger("Attack");
+        }
     }
 }
