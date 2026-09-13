e407e13020832ff69318094053bab8282e9ad094
Sebastian K <sebastian.king19@education.nsw.gov.au>
2026-09-14 09:40:13 +1000

    WIP enemy hurt  + death

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -5,25 +5,31 @@ using UnityEngine.InputSystem;
 
 public class EnemyHealth : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
     public float health;
     private float hurt;
-    public ParticleSystem particleSystem;
-    IEnumerator Pause1sec()
+    IEnumerator Pause15sec()
     {
-        yield return new WaitForSeconds (1f);
+        yield return new WaitForSeconds (5f);
     }
     
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
         if(health <= 0)
         {
             Debug.Log("enemy is dead");
+            Destroy(gameObject);
         }
 
         if (hurt != health)
         {
-            particleSystem.Play();
-            Pause1sec();
+            spriteRenderer.color = Color.red;
+            Pause15sec();
+            spriteRenderer.color = Color.white;
             hurt = health;
         } 
     }
